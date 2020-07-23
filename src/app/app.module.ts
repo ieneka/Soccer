a9762aae025c5636d95308230f784d9cb1897aa6
Blanca Frade <blancafradep@gmail.com>
2020-07-23 13:27:09 +0200

    added landing view comopnent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingViewComponent } from './views/landing-view/landing-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingViewComponent
   ],
   imports: [
     BrowserModule,
