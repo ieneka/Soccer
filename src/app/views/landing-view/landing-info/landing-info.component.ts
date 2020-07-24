07b424400615ea110ee921021df79718415a6583
Bassam AL SAEDY <basaidi@gmail.com>
2020-07-24 15:31:30 +0200

    Landing view(html & ts) components modified!

--- src/app/views/landing-view/landing-info/landing-info.component.ts
+++ src/app/views/landing-view/landing-info/landing-info.component.ts
@@ -10,7 +10,7 @@ export class LandingInfoComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  // Adding Images
   images = [
     'assets/Images/Landing/P1.jpg',
     'assets/Images/Landing/P2.jpg',
