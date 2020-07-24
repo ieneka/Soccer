e9f95c7e4b8541b06d6c55b753c1f75ebe89869c
Carlos Roth <>
2020-07-24 16:30:36 +0200

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingViewComponent } from './views/landing-view/landing-view.component';
+import { SignUpComponent } from './views/landing-view/sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LandingViewComponent
-  ],
+  declarations: [AppComponent, LandingViewComponent, SignUpComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
