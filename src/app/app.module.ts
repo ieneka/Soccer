157b4c08cc3f8bb4b1d26d4691f31b46205ed6dc
Blanca Frade <blancafradep@gmail.com>
2020-07-27 09:02:42 +0200

    declaration of components & imported reactiveForms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingViewComponent } from './views/landing-view/landing-view.component';
+import { HeaderComponent } from './views/landing-view/header/header.component';
+import { ForgotPasswordComponent } from './views/landing-view/header/forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingViewComponent
+    LandingViewComponent,
+    HeaderComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
