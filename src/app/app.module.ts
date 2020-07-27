5274b0381a63a03aab60a3300e05ce5e9c61f531
Blanca Frade <blancafradep@gmail.com>
2020-07-27 11:48:07 +0200

    added managers and single manager components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingViewComponent } from './views/landing-view/landing-view.component';
+import { ManagersViewComponent } from './views/admin/managers-view/managers-view.component';
+import { SingleManagerViewComponent } from './views/admin/single-manager-view/single-manager-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingViewComponent
+    LandingViewComponent,
+    ManagersViewComponent,
+    SingleManagerViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/admin/managers-view/managers-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-managers-view',
+  templateUrl: './managers-view.component.html',
+  styleUrls: ['./managers-view.component.css']
+})
+export class ManagersViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
