05c52e2973ca281458f92b13d28daf41b6bad0c0
Blanca Frade <blancafradep@gmail.com>
2020-07-30 10:25:49 +0200

    added admin.module&admin.routing update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingViewComponent } from './views/landing-view/landing-view.component';
-import { ManagersViewComponent } from './views/admin/managers-view/managers-view.component';
-import { SingleManagerViewComponent } from './views/admin/single-manager-view/single-manager-view.component';
-import { UserListComponentComponent } from './views/admin/managers-view/user-list-component/user-list-component.component';
+
 import { SignUpComponent } from './views/landing-view/sign-up/sign-up.component';
 import { LandingFooterComponent } from './views/landing-view/landing-footer/landing-footer.component';
 import { LandingInfoComponent } from './views/landing-view/landing-info/landing-info.component';
@@ -15,22 +13,19 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { HeaderComponent } from './views/landing-view/header/header.component';
 import { ForgotPasswordComponent } from './views/landing-view/header/forgot-password/forgot-password.component';
-import { ManagerInfoComponent } from './views/admin/single-manager-view/manager-info/manager-info.component';
+
+import { AdminModule } from './views/admin/admin.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingViewComponent,
-    ManagersViewComponent,
-    SingleManagerViewComponent,
-    UserListComponentComponent,
     SignUpComponent,
     LandingInfoComponent, 
     PageNotFoundComponent,
     LandingFooterComponent,
     HeaderComponent,
     ForgotPasswordComponent,
-    ManagerInfoComponent
   ],
   imports: [
     BrowserModule,
@@ -38,7 +33,8 @@ import { ManagerInfoComponent } from './views/admin/single-manager-view/manager-
     ReactiveFormsModule,
     NgbModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/views/admin/single-manager-view/manager-info/manager-info.component.ts
+++ src/app/views/admin/single-manager-view/manager-info/manager-info.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { FormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @Component({
@@ -8,17 +8,19 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./manager-info.component.css']
 })
 export class ManagerInfoComponent implements OnInit {
+  @ViewChild('select') selectElRef;
 
   email: string;
   password: string;
   team_name: string;
   resultado: string;
 
-  players = [
-    'Player1',
-    'Player2',
-    'Player3',
-  ];
+  myOptions = [ 
+    {value: 1, name: "Player1"}, 
+    {value: 2, name: "Player2"},
+    {value: 3, name: "Player3"}];
+  selectedValues = ['1','2'];
+  myModelProperty = this.myOptions[0];
 
   constructor() { }
 
@@ -31,6 +33,26 @@ export class ManagerInfoComponent implements OnInit {
     team_name: new FormControl('', [Validators.required]),
   });
 
+  ngAfterViewInit() {
+    this.updateSelectList();
+  }
+
+  updateSelectList() {
+    let options = this.selectElRef.nativeElement.options;
+    for(let i=0; i < options.length; i++) {
+      options[i].selected = this.selectedValues.indexOf(options[i].value) > -1;
+    }
+  }
+  change(options) {
+    this.selectedValues = Array.apply(null,options)  // convert to real Array
+      .filter(option => option.selected)
+      .map(option => option.value)
+  }
+  changeOptions() {
+    this.selectedValues = ['1','3'];
+    this.updateSelectList();
+  }
+
   /**
    * Process the form we have. Send to whatever backend
    * Only alerting for now
@@ -38,11 +60,12 @@ export class ManagerInfoComponent implements OnInit {
   processForm() {
     if (this.formularioAdminManager.valid) {
 
-      let playersSelected: String[] = this.formularioAdminManager.controls['players'].value;
-      for (let i=0; i < playersSelected.length; i++) {
-        console.log("Player: " + playersSelected[i]);
-      } 
-      const allInfo = `Username: ${this.email}. Pwd: ${this.password}. Team Name: ${this.team_name}.`;
+      // console.log(this.formularioAdminManager.controls)
+      // let playersSelected: String[] = this.formularioAdminManager.controls['players'].value;
+      // for (let i=0; i < playersSelected.length; i++) {
+      //   this.resultado = "Player: " + playersSelected[i];
+      // } 
+      const allInfo = `Username: ${this.email}. Pwd: ${this.password}. Team Name: ${this.team_name} . ${this.selectedValues}`;
       this.resultado = allInfo; 
     } else {
       this.resultado = "Hay datos inválidos en el formulario";
