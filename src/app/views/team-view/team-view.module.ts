151b21771d20603277adcd764a055ee3e325418c
Bassam AL SAEDY <basaidi@gmail.com>
2020-09-16 17:05:32 +0200

    Best pleayers layout added!

--- src/app/views/team-view/team-view.module.ts
+++ src/app/views/team-view/team-view.module.ts
@@ -4,9 +4,16 @@ import { TeamViewComponent } from './team-view/team-view.component';
 import { BadgeComponent } from './team-view/badge/badge.component';
 import { TeamInfoComponent } from './team-view/team-info/team-info.component';
 import { LastFiveResultsComponent } from './team-view/last-five-results/last-five-results.component';
+import { BestPlayersLayoutComponent } from './team-view/best-players-layout/best-players-layout.component';
 
 @NgModule({
-  declarations: [TeamViewComponent, BadgeComponent, TeamInfoComponent, LastFiveResultsComponent],
+  declarations: [
+    TeamViewComponent,
+    BadgeComponent,
+    TeamInfoComponent,
+    LastFiveResultsComponent,
+    BestPlayersLayoutComponent,
+  ],
   imports: [CommonModule],
   exports: [TeamViewComponent],
 })
