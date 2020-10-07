5fd2d0b8d8ccbcbd6afbb8268040dee8d55fe803
Vedranaha <vedranaha@gmail.com>
2020-10-07 22:56:56 +0200

    userlist service corrected

--- src/app/services/userlist.service.ts
+++ src/app/services/userlist.service.ts
@@ -3,20 +3,33 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Manager } from '../views/admin/Managers/managers.service';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserListService {
 
-  private usersUrl = 'api/users';  // URL to web api
-  constructor(private http: HttpClient) {
+  private usersUrl = 'http://217.76.158.200:8181/api/users';  // URL to web api
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  constructor(private http: HttpClient)
+  {
     console.log('service ready');
   }
 
 /** GET users from the server */
 getUsers(): Observable<Manager[]> {
   return this.http.get<Manager[]>(this.usersUrl)
-}}
+}
+ //////// Save methods //////////
+
+  /** POST: add a new user to the server */
+  addUser(user: User): Observable<User> {
+    return this.http.post<User>(this.usersUrl, user, this.httpOptions);
+  }
+}
 
 export interface User {
   id: number;
