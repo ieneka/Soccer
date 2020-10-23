2378e53c6a49972e9c36b241032d774860cf12c2
oihoihoih <ochyuo@hotmail.com>
2020-10-23 12:54:43 +0200

    In range slider, default value visualization implementation

--- src/app/views/market-view/skill-filter/skill-filter.component.ts
+++ src/app/views/market-view/skill-filter/skill-filter.component.ts
@@ -8,14 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class SkillFilterComponent implements OnInit {
 
   years: number[];
+  rangeAttack: number = 5;
+  rangeDefense: number = 5;
+  rangePass: number = 5;
+  rangeKeeper: number = 5;
+
 
   constructor() {
 
     this.years = new Array ( 100 ).fill( 0 ).map( (x, i) => i + 1);
-    
+
   }
 
   ngOnInit(): void {
   }
 
+  valueAttack(e) {
+    console.log(e);
+    this.rangeAttack = e.target.value;
+  }
+  valueDefense(e) {
+    console.log(e);
+    this.rangeDefense = e.target.value;
+  }
+  valuePass(e) {
+    console.log(e);
+    this.rangePass = e.target.value;
+  }
+  valueKeeper(e) {
+    console.log(e);
+    this.rangeKeeper = e.target.value;
+  }
 }
